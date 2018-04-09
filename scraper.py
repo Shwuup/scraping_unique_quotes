9693c21064edf21579f6bb1a721a0e6643dd5ad1
Shwuup <e_kong98z@hotmail.com>
2018-04-09 20:27:33 +1000

    FASTER!
    Used inbuilt sorting function and binary search to make comparisons between scraped quotes and unique quotes faster.

--- scraper.py
+++ scraper.py
@@ -1,11 +1,25 @@
 import json, re
+from Quote import *
+
+def bin_search(l, target):
+    start = 0
+    end = len(l) - 1
+    for stuff in range(len(l)):
+        middle = (start + end) // 2
+        if l[middle] == target:
+            return middle
+        elif l[middle ] < target:
+            start = middle + 1
+        elif l[middle] > target:
+            end = middle
+    return -1
 
 def make_quote_json(filename, len_unique_quotes):
     quotes_author_pairs = []
     for stuff in range(len_unique_quotes):
         dic = {}
-        dic["quote"] = unique_quotes[stuff][0] + "."
-        dic["author"] = unique_quotes[stuff][1]
+        dic["quote"] = unique_quotes[stuff].quote
+        dic["author"] = unique_quotes[stuff].author
         quotes_author_pairs.append(dic)
         if (stuff + 1) % 20 == 0:   #make new json file every 20 quotes
             filename += 1
@@ -20,6 +34,7 @@ def json_maker(filename, quotes_authors):
     data["data"] = quotes_authors
     with open(str(filename) + '.json', 'w') as outfile:
         json.dump(data, outfile, indent=4)
+
 
 quote_file = open("quote_list.txt", encoding='utf-8')
 quote_list = []
@@ -43,23 +58,27 @@ for stuff in range(len(quote_list)):
     quote_list[stuff] = authorRegex.sub("", quote_list[stuff])
     quote_list[stuff] = rhsQuotationRegex.sub("\u2019", quote_list[stuff])
     quote_list[stuff] = lhsQuotationRegex.sub("\u2018", quote_list[stuff])
+    quote_list[stuff] = quote_list[stuff] + "."
 
+quote_list_class = []
+unique_quotes = []
+for n in range(len(quote_list)):
+    quote_list_class.append(Quote(quote_list[n], authors_list[n]))
+
+their_list = []
 #load up the json file
 json_quotes = json.load(open('unique_quotes.json'))
 
-#iterate through quote list, and compare to every other unique quote in json file
-unique_quotes = []
-for our_quotes in range(len(quote_list)):
-    quoteRegex = re.compile(quote_list[our_quotes])
-    found = False
-    for their_quotes in json_quotes['data']:
-        match = quoteRegex.search(their_quotes["quote"])
-        if match is not None:
-            found = True
-            break
-    if found is False:
-        unique_quotes.append((quote_list[our_quotes], authors_list[our_quotes])) #tuple with unique quote and author's name
 
-make_quote_json(157, len(unique_quotes))
+for n in range(len(json_quotes['data'])):
+    their_list.append(json_quotes['data'][n]['quote'])
+
+their_list = sorted(their_list)
+for q in range(len(quote_list_class)):
+    if bin_search(their_list, quote_list_class[q].quote) == -1:
+        unique_quotes.append(quote_list_class[q])
+
+make_quote_json(161, len(unique_quotes))
+
 
 
